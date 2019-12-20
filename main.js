edb2a71314cf117caa130c093b632f716fc350ea
pastre <pastre68@gmail.com>
2019-12-20 02:24:38 -0300

    Created basic game CRUD for players

--- main.js
+++ main.js
@@ -1,19 +1,116 @@
- const http = require('http'); 
+const removeArrayItem = (arr, itemToRemove) => {
+  return arr.filter(item => item !== itemToRemove)
+}
 
-  console.log('a');
-var io = require('socket.io');
-var server = http.createServer();
-server.listen(8080, 'localhost');
-var socket = io(server);
 
-var ioServer = socket
+class Position {
+	constructor(x, y) {
+		this._x = x
+		this._y = y
+	}
+}
 
-ioServer.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+class Player {
+	constructor(id, position) {
+		this._id = id
+		this._position = position
+	}
+}
+
+class Game {
+
+	constructor() {
+		this.players = []
+	}
+
+
+	update() {
+		console.log("Players are", this.players)
+		// this.players.forEach((item, index, arr) => )
+	}
+
+	addPlayer(player) {
+		this.players.push(player)
+	}
+
+	removePlayer(player){
+		this.players = this.players.filter((p) => p._id !== player._id)
+	}
+
+}
+
+class NetworkGame {
+
+	constructor() {
+		this.lastUpdate = new Date()
+		this.game = new Game()
+		this.connections = {}
+		this.currentPlayerId = 0
+	}
+
+
+	getUpdatedGame() {
+		this.game.update()
+
+		console.log("Connections are", this.connections)
+	}
+
+	parseGame() {
+
+	}
+
+	playerJoined(socket) {
+		console.log("CORNO 	ENTROU")
+		const newPlayer = new Player(this.currentPlayerId, new Position(0, 0))
+
+		this.game.addPlayer(newPlayer)
+
+		this.currentPlayerId += 1
+
+		return newPlayer._id
+	
+}
+	playerLeft(id) {
+		console.log("CORNO 	SAIU", id)
+		const leftPlayer = this.connections.socket
+
+		this.game.players.forEach( (player, index, _) => {
+			if(player._id === id) {
+				console.log("Brow to saindo fora")
+				this.game.removePlayer(player)
+
+			}
+		}
+		)
+
+
+	}
+}
+
+const game = new NetworkGame()
+const http = require('http'); 
+const io = require('socket.io');
+const server = http.createServer();
+server.listen(8080, '0.0.0.0');
+const serverSocket = io(server);
+
+
+serverSocket.on('connection', (clientSocket) => {
+	let newPlayerId = game.playerJoined(clientSocket)
+	clientSocket.on('disconnect', () => game.playerLeft(newPlayerId));
 });
 
+serverSocket.on('directionChanged', (clientSocket) => {
 
-  console.log('a');
-setInterval(() => ioServer.emit('time', new Date().toTimeString()), 1000);
-  console.log('a');
+});
+
+// setInterval(() => socket.emit('time', new Date().toTimeString()), 1000);
+
+
+setInterval(updateGame, 1000);
+
+
+function updateGame() {
+	game.getUpdatedGame()
+	serverSocket.emit('time', new Date().toTimeString());
+}
