4146bfdca5ea77b240d2a816e9bc2e723fcb88af
Bruno Pastre <pastre68@gmail.com>
2019-12-21 21:09:35 -0300

    Added basic needle code

--- main.js
+++ main.js
@@ -12,12 +12,64 @@ class Position {
 
 }
 
+class Needle {
+	constructor(position) {
+		this.position = position
+		this.offset = new Position(0, 0)
+		this.accDeltaTime = 0
+		this.isAttacking = false
+		this.movementLenght = 2
+		this.movementDuration = 1000
+	}
+
+	resetNeedle() {
+		this.accDeltaTime = 0
+		this.offset = new Position(0, 0)
+		this.isAttacking = false
+	}
+
+	update(timeDelta, position) {
+		if(!this.isAttacking){
+			this.position = position
+			return 
+		}
+
+		if this.accDeltaTime > this.movementDuration {
+			this.resetNeedle()
+			return
+		}
+
+		this.position = getNeedlePosition(position)
+
+		this.accDeltaTime += timeDelta
+	}
+
+	getNeedlePosition(playerPos) {
+		let completion = this.accDeltaTime / this.movementDuration
+		var multiplier
+		if (completion < 0.5) {
+			multiplier = completion * 2
+		} else {
+			multiplier = completion - 2*(completion - 0.5)
+		}
+
+		let needleX = playerPos.x + this.movementLenght * multiplier
+		let needleY = playerPos.Y + this.movementLenght * multiplier
+
+		return new Position(needleX, needleY)
+	}
+
+}
+
 class Player {
 	constructor(id, position) {
 		this.id = id
 		this.position = position
 		this.direction = Math.random() * (Math.PI) * 2 //0
 		this.speed = 0 //0.05
+		this.radius = 2
+		this.needle = new Needle(this.getNeedlePosition())
+	
 	}
 
 	calcSpaceDelta(timeDelta, s0) {
@@ -29,14 +81,22 @@ class Player {
 		let newX = this.calcSpaceDelta(timeDelta, this.position.x)
 		let newY = this.calcSpaceDelta(timeDelta, this.position.y)
 
-		// console.log("NN", newY, newX, this.position.x, this.position.y, this.speed)
-
 		this.position.x += newX * Math.cos(this.direction)
 		this.position.y += newX * Math.sin(this.direction)
 	}
 
 	update(timeDelta) {
 		this.updatePosition(timeDelta)
+		this.needle.update(timeDelta, this.getNeedlePosition())
+		
+	}
+
+	attack() {
+		this.needle.isAttacking = true
+	}
+
+	getNeedlePosition() {
+		return new Position(this.position.x + this.radius * Math.cos(this.direction.x), this.position.y + this.radius * Math.sin(this.direction.y) )
 	}
 }
 
@@ -88,6 +148,17 @@ class Game {
 		})
 	}
 
+	updatePlayerAttack(playerId) {
+		this.players.forEach(function (player) {
+			// console.log("DD", player.id, playerId)
+			if( player.id === playerId) {
+				// console.log("DD", playerId, to)
+				player.attack()
+			}	
+		})
+
+	}
+
 
 }
 
@@ -137,6 +208,10 @@ class NetworkGame {
 		// console.log("Direction changed", to, id)
 		this.game.updatePlayerSpeed(id, to)
 	}
+
+	playerAttacked(id) {
+		this.game.updatePlayerAttack(id)
+	} 
 }
 
 const PORT = process.env.PORT || 3000;
@@ -165,6 +240,10 @@ serverSocket.on('connection', (clientSocket) => {
 
 	clientSocket.on('speedChanged', (msg) => {
 		game.playerChangedSpeed(newPlayerId, msg)
+	});
+
+	clientSocket.on('startAttack', (msg) => {
+		game.playerAttacked(newPlayerId)
 	});
 
 	clientSocket.on('disconnect', () => game.playerLeft(newPlayerId));
